69cdbf32f50d42b6223ff0d684ccf7e92b1bcc1d
geethasreepada <59782179+geethasreepada@users.noreply.github.com>
2020-02-09 00:55:13 -0800

    Update Candy.java

--- Candy.java
+++ Candy.java
@@ -1,11 +1,11 @@
 public class Candy extends DesertItem{
 
     private double weight;
-    private int pricePerPound;
-    public Candy(String itemName, double weight, int pricePerPound) {
+    private int priceperpound;
+    public Candy(String itemName, double weight, int priceperPound) {
         super(itemName);
         this.weight=weight;
-        this.pricePerPound=pricePerPound;
+        this.priceperPound=priceperPound;
     }
 
     public int getCost() {
@@ -17,7 +17,7 @@ public class Candy extends DesertItem{
 
     public String toString(){
 
-        String costDesc = weight+" lbs. @ "+DesertShoppe.cents2dollarsAndCentsmethod(pricePerPound)+" /lb " + "\n";
+        String costDesc = weight+" lbs. @ "+DesertShoppe.cents2dollarsAndCentsmethod(priceperpound)+" /lb " + "\n";
         String cost = DesertShoppe.cents2dollarsAndCentsmethod(getCost());
         String formattedName = DesertShoppe.printItemInReceipt(name, cost);
         return costDesc + formattedName;
